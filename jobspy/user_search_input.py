7c8eb1c8458619810d8d2b6db6d5d17b7b78395c
bart <bielski.bart1012@gmail.com>
2025-08-08 09:38:59 +0100

    move user search data to json

--- jobspy/user_search_input.py
+++ jobspy/user_search_input.py
@@ -1,5 +1,6 @@
 from jobspy.model import JobCategory
 
+
 job_titles = [
     "Software",
     "Graduate",
@@ -15,6 +16,36 @@ job_titles = [
     "Web Content Administrator",
     "Apprentice",
 ]
+
+inclusion_keywords = [
+    "software engineer",
+    "software developer",
+    "junior developer",
+    "junior software engineer",
+    "web developer",
+    "frontend developer",
+    "backend developer",
+    "full stack developer",
+    "entry level developer",
+    "graduate developer",
+    "developer",
+    "programmer",
+    "data analyst",
+    "technical writer",
+    "digital marketing",
+    "content creator",
+    "marketing assistant",
+    "marketing executive",
+    "communications assistant",
+    "digital content",
+    "social media",
+    "ux",
+    "ui",
+    "product designer",
+    "content designer",
+    "copywriter",
+]
+
 
 target_job_categories = [
     JobCategory.SALES,
--- main.py
+++ main.py
@@ -35,46 +35,54 @@ temp_df = None
 
 
 # Scrape Govuk for all job categories defined by user
-for job_category in user_search_input.target_job_categories:
+# for job_category in user_search_input.target_job_categories:
+
+#     jobs_df = scrape_jobs(
+#         site_name=["govuk"],
+#         govuk_job_category=job_category,
+#         govuk_location=LocationCode.NORTHWEST,
+#         avoid_keywords='senior Fundraiser Scientist Executive Transport Steward head Painter "Teaching Assistant" "Self Employed" "Store Assistant" "self-employed" "Team Member" Lecturer Associate Officer Lead Estimator Teaching principal Coach ITOL beBeeSoftware milk waiter retail chef mechanic van manager therapist nurse bar care housekeeper MOT Recruitment Shift Factory Warehouse',
+#         results_wanted=5,
+#     )
+
+#     temp_df = jobs_df if temp_df is None else pd.concat([temp_df, jobs_df])
+
+for job_title in user_search_input.job_titles:
 
     jobs_df = scrape_jobs(
-        site_name=["govuk"],
-        govuk_job_category=job_category,
-        govuk_location=LocationCode.NORTHWEST,
-        avoid_keywords='senior Fundraiser Scientist Executive Transport Steward head Painter "Teaching Assistant" "Self Employed" "Store Assistant" "self-employed" "Team Member" Lecturer Associate Officer Lead Estimator Teaching principal Coach ITOL beBeeSoftware milk waiter retail chef mechanic van manager therapist nurse bar care housekeeper MOT Recruitment Shift Factory Warehouse',
-        results_wanted=1,
+        site_name=["indeed", "linkedin"],
+        search_term=f'"{job_title}"',
+        avoid_keywords='Sr. Care Finance "VITA CV" Lecturer Warehouse Dentist Manager Accountant Operator Childcare Security Leader Kitchen Teaching DataAnnotation Painter TieTalent Senior "Bending Spoons" Surveyor Teaching Joiner Lead Executive',
+        location="Liverpool, Merseyside",
+        linkedin_fetch_description=True,
+        distance=50,
+        results_wanted=10,
+        hours_old=24 * 7,
+        country_indeed="UK",
     )
+    if len(jobs_df) > 0:
+        cleaned_df = jobs_df[
+            ["title", "company", "location", "date_posted", "site", "job_url", "id"]
+        ]
+        add_to_spreadsheet(cleaned_df, worksheet)
 
-    temp_df = jobs_df if temp_df is None else pd.concat([temp_df, jobs_df])
 
-# for job_title in user_search_input.job_titles:
+# temp_df = jobs_df if temp_df is None else pd.concat([temp_df, jobs_df])
 
-jobs_df = scrape_jobs(
-    site_name=["indeed", "linkedin"],
-    search_term=f'"Graduate"',
-    avoid_keywords='Care Finance Lecturer Warehouse Dentist Manager Accountant Operator Childcare Security Leader Kitchen Teaching DataAnnotation Painter TieTalent Senior "Bending Spoons" Surveyor Teaching Joiner Lead Executive',
-    location="Liverpool, Merseyside",
-    linkedin_fetch_description=True,
-    distance=50,
-    results_wanted=1,
-    hours_old=24 * 7,
-    country_indeed="UK",
-)
-temp_df = jobs_df if temp_df is None else pd.concat([temp_df, jobs_df])
+# cleaned_df = jobs_df[
+#     ["title", "company", "location", "date_posted", "site", "job_url", "id"]
+# ]
+# print(f"Length prior to AI filtering: {len(cleaned_df)}")
 
-cleaned_df = temp_df[
-    ["title", "company", "location", "date_posted", "site", "job_url", "id"]
-]
-print(f"Length prior to AI filtering: {len(cleaned_df)}")
+# response = genAI_client.models.generate_content(
+#     model="gemini-2.5-flash",
+#     contents=f"I have a number of scraped jobs. Despite best efforts to filter out irrelevant entries, some still persist. Your task is to filter out all irrelevant jobs based on the CV provided; The jobs you should omit include all irrelevant jobs to my education, skills, and experience, as well as any role with a high seniority level such as manager or executive. My CV: {user_cv.user_cv}. Jobs: {cleaned_df}",
+#     config={
+#         "response_mime_type": "application/json",
+#         "response_schema": list[Job_short],
+#     },
+# )
 
-response = genAI_client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=f"I have a number of scraped jobs. Despite best efforts to filter out irrelevant entries, some still persist. Your task is to filter out all irrelevant jobs based on the CV provided; The jobs you should omit include all irrelevant jobs to my education, skills, and experience, as well as any role with a high seniority level such as manager or executive. My CV: {user_cv.user_cv}. Jobs: {cleaned_df}",
-    config={
-        "response_mime_type": "application/json",
-        "response_schema": list[Job_short],
-    },
-)
-
-filtered_df = pd.read_json(response.text)
-add_to_spreadsheet(filtered_df, worksheet)
+# filtered_df = pd.read_json(response.text)
+# add_to_spreadsheet(cleaned_df, worksheet)
+#
